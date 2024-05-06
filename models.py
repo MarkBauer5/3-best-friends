d7390ecda8254caaf31bf9ef147c46fac232792d
keriotisna <82786494+keriotisna@users.noreply.github.com>
2024-05-06 09:12:07 -0500

    Add final models and new training stats

--- models.py
+++ models.py
@@ -29,6 +29,7 @@ from modelUtils import profileModel
 
 # Gets 86.55% on test set
 # TODO: This is pretty sketchy, maybe there's a better way to get this model untrained but I don't know of it.
+# 94.942 Train, 85.945 Val, 86.257 Test 7367s training
 # class VisualizableVIT(nn.Module):
 #     def __init__(self):
 #         super().__init__()
@@ -57,60 +58,62 @@ from modelUtils import profileModel
 
 
 # 93.73% Test 94.06% Validation
-# class VisualizableSWIN(nn.Module):
+# 92.206 Train, 94.515 Val 94.485 Test 10096s training
+# 97.84 Train 94.955 Val, 96.215 Test Took 7367s to train 
+class VisualizableSWIN(nn.Module):
     
-#     def __init__(self):
+    def __init__(self):
         
-#         super(VisualizableSWIN, self).__init__()
+        super(VisualizableSWIN, self).__init__()
         
-#         class Args:
-#             def __init__(self) -> None:
-#                 # data arguments
-#                 self.num_classes = 2
-#                 self.img_size = 256
-#                 self.num_train_data = 10000
-#                 self.num_test_data = 2000
-#                 self.dataset_path = "datasets/140k Real vs Fake/real_vs_fake/real-vs-fake/"
+        class Args:
+            def __init__(self) -> None:
+                # data arguments
+                self.num_classes = 2
+                self.img_size = 256
+                self.num_train_data = 10000
+                self.num_test_data = 2000
+                self.dataset_path = "datasets/140k Real vs Fake/real_vs_fake/real-vs-fake/"
 
-#                 # training arguments
-#                 self.learning_rate =  1e-4
-#                 self.epochs = 10
-#                 self.scheduler = True
-#                 self.sch_step_size = 2
-#                 self.sch_gamma = 0.1
+                # training arguments
+                self.learning_rate =  1e-4
+                self.epochs = 10
+                self.scheduler = True
+                self.sch_step_size = 2
+                self.sch_gamma = 0.1
 
-#                 # model arguments
-#                 self.drop_path_rate = 0.2
-#                 self.embed_dim = 96
-#                 self.depths = (2, 2, 6, 2)
-#                 self.num_heads = (3, 6, 12, 24)
-#                 self.window_size = 16
-#                 self.load_model_path = "CollectedData/Models/swinv2_tiny_patch4_window16_256.pth"
-#                 self.save_model_path = "CollectedData/Models/swinv2_tiny_patch4_window16_256.pth"
+                # model arguments
+                self.drop_path_rate = 0.2
+                self.embed_dim = 96
+                self.depths = (2, 2, 6, 2)
+                self.num_heads = (3, 6, 12, 24)
+                self.window_size = 16
+                self.load_model_path = "CollectedData/Models/swinv2_tiny_patch4_window16_256.pth"
+                self.save_model_path = "CollectedData/Models/swinv2_tiny_patch4_window16_256.pth"
 
-#                 # output arguments
-#                 self.output_path = "../output/"
+                # output arguments
+                self.output_path = "../output/"
 
-#         args = Args()
+        args = Args()
 
-#         _visualizableSWIN = SwinTransformerV2(img_size=args.img_size,
-#                                 drop_path_rate=args.drop_path_rate,
-#                                 embed_dim=args.embed_dim,
-#                                 depths=args.depths,
-#                                 num_heads=args.num_heads,
-#                                 window_size=args.window_size)
-#         state_dict = torch.load(args.load_model_path)
-#         _visualizableSWIN.load_state_dict(state_dict["model"])
-#         _visualizableSWIN.head = torch.nn.Linear(_visualizableSWIN.head.in_features, args.num_classes)
+        _visualizableSWIN = SwinTransformerV2(img_size=args.img_size,
+                                drop_path_rate=args.drop_path_rate,
+                                embed_dim=args.embed_dim,
+                                depths=args.depths,
+                                num_heads=args.num_heads,
+                                window_size=args.window_size)
+        state_dict = torch.load(args.load_model_path)
+        _visualizableSWIN.load_state_dict(state_dict["model"])
+        _visualizableSWIN.head = torch.nn.Linear(_visualizableSWIN.head.in_features, args.num_classes)
         
-#         # Reset model parameters to train from scratch
-#         for layer in _visualizableSWIN.children():
-#             layer.apply(_visualizableSWIN._init_weights)
+        # Reset model parameters to train from scratch
+        for layer in _visualizableSWIN.children():
+            layer.apply(_visualizableSWIN._init_weights)
             
-#         self.model = _visualizableSWIN
+        self.model = _visualizableSWIN
         
-#     def forward(self, x):
-#         return self.model(x)
+    def forward(self, x):
+        return self.model(x)
 
 
 
@@ -509,147 +512,95 @@ from modelUtils import profileModel
 # TODO: Try a better CNN lul
 
 # 0.9999 Train, 0.9151 Val, 0.91585 Test
-# customResnet = nn.Sequential(
-#     ResidualBlock4(in_channels=3, out_channels=4),
-#     ResidualDownsample(in_channels=4), # 112
+# Augmentation: 90.256|89.385|89.72
+customResnet = nn.Sequential(
+    ResidualBlock4(in_channels=3, out_channels=4),
+    ResidualDownsample(in_channels=4), # 112
     
-#     ResidualBlock4(in_channels=4, out_channels=8),
-#     ResidualDownsample(in_channels=8), # 56
+    ResidualBlock4(in_channels=4, out_channels=8),
+    ResidualDownsample(in_channels=8), # 56
     
-#     ResidualBlock4(in_channels=8, out_channels=16),
-#     ResidualBlock4(in_channels=16, out_channels=32),
-#     ResidualDownsample(in_channels=32), # 28
+    ResidualBlock4(in_channels=8, out_channels=16),
+    ResidualBlock4(in_channels=16, out_channels=32),
+    ResidualDownsample(in_channels=32), # 28
     
-#     ResidualBlock4(in_channels=32, out_channels=64),
-#     ResidualBlock4(in_channels=64, out_channels=128),
-#     ResidualBlock4(in_channels=128, out_channels=128),
-#     ResidualDownsample(in_channels=128), # 14
+    ResidualBlock4(in_channels=32, out_channels=64),
+    ResidualBlock4(in_channels=64, out_channels=128),
+    ResidualBlock4(in_channels=128, out_channels=128),
+    ResidualDownsample(in_channels=128), # 14
 
-#     ResidualBlock4(in_channels=128, out_channels=256),
-#     *[ResidualBlock4(in_channels=256, out_channels=256) for _ in range(2)],
-#     ResidualDownsample(in_channels=256), # 7
+    ResidualBlock4(in_channels=128, out_channels=256),
+    *[ResidualBlock4(in_channels=256, out_channels=256) for _ in range(2)],
+    ResidualDownsample(in_channels=256), # 7
     
-#     ResidualBlock4(in_channels=256, out_channels=512),
-#     *[ResidualBlock4(in_channels=512, out_channels=512) for _ in range(2)],
-#     ResidualDownsample(in_channels=512), # 3
+    ResidualBlock4(in_channels=256, out_channels=512),
+    *[ResidualBlock4(in_channels=512, out_channels=512) for _ in range(2)],
+    ResidualDownsample(in_channels=512), # 3
 
-#     ResidualBlock4(in_channels=512, out_channels=1024),
-#     *[ResidualBlock4(in_channels=1024, out_channels=1024) for _ in range(2)],
+    ResidualBlock4(in_channels=512, out_channels=1024),
+    *[ResidualBlock4(in_channels=1024, out_channels=1024) for _ in range(2)],
 
-#     *[ResidualBlock4(in_channels=1024, out_channels=1024) for _ in range(3)],
+    *[ResidualBlock4(in_channels=1024, out_channels=1024) for _ in range(3)],
 
-#     ResidualBlock4(in_channels=1024, out_channels=2048),
-#     *[ResidualBlock4(in_channels=2048, out_channels=2048) for _ in range(3)],
+    ResidualBlock4(in_channels=1024, out_channels=2048),
+    *[ResidualBlock4(in_channels=2048, out_channels=2048) for _ in range(3)],
 
-#     ResidualBlock4(in_channels=2048, out_channels=4096),
-#     *[ResidualBlock4(in_channels=4096, out_channels=4096) for _ in range(2)],
-
-#     nn.AdaptiveAvgPool2d(1),
-#     nn.Flatten(),
-    
-#     nn.Linear(in_features=4096, out_features=2048),
-#     nn.ReLU(),
-#     nn.LayerNorm(normalized_shape=2048),
-    
-#     nn.Linear(in_features=2048, out_features=1024),
-#     nn.ReLU(),
-#     nn.LayerNorm(normalized_shape=1024),
-
-#     nn.Linear(in_features=1024, out_features=512),
-#     nn.ReLU(),
-#     nn.LayerNorm(normalized_shape=512),
-    
-#     nn.Linear(in_features=512, out_features=2),
-# )
-
-# Runs like twice as fast as previous ResNet, but looks like it will have worse performance# Also only uses 9.2 GB when compared to the 22 GB from ResNet 
-# TODO: Continue training this later, seems to not have fully fit yet.
-# After 15 epochs: Train: 92.414%, Val: 81.015%, Test: 81.035%
-superSepNetSmall = nn.Sequential(
-    DoubleResidualDWSeparableConv2d(in_channels=3, out_channels=9),
-    ResidualDownsampleSep(in_channels=9), # 112
-    
-    DoubleResidualDWSeparableConv2d(in_channels=9, out_channels=18),
-    ResidualDownsampleSep(in_channels=18), # 56
-    
-    DoubleResidualDWSeparableConv2d(in_channels=18, out_channels=36),
-    ResidualDownsampleSep(in_channels=36), # 28
-    
-    DoubleResidualDWSeparableConv2d(in_channels=36, out_channels=72),
-    DoubleResidualDWSeparableConv2d(in_channels=72, out_channels=144),
-    DoubleResidualDWSeparableConv2d(in_channels=144, out_channels=144),
-    ResidualDownsampleSep(in_channels=144), # 14
-
-    DoubleResidualDWSeparableConv2d(in_channels=144, out_channels=288),
-    *[DoubleResidualDWSeparableConv2d(in_channels=288, out_channels=288) for _ in range(2)],
-    ResidualDownsampleSep(in_channels=288), # 7
-    
-    DoubleResidualDWSeparableConv2d(in_channels=288, out_channels=576),
-    *[DoubleResidualDWSeparableConv2d(in_channels=576, out_channels=576) for _ in range(2)],
-    ResidualDownsampleSep(in_channels=576), # 3
-
-    DoubleResidualDWSeparableConv2d(in_channels=576, out_channels=1152),
-    *[DoubleResidualDWSeparableConv2d(in_channels=1152, out_channels=1152) for _ in range(5)],
-
-    DoubleResidualDWSeparableConv2d(in_channels=1152, out_channels=2304),
-    *[DoubleResidualDWSeparableConv2d(in_channels=2304, out_channels=2304) for _ in range(3)],
-
-    DoubleResidualDWSeparableConv2d(in_channels=2304, out_channels=4608),
-    *[DoubleResidualDWSeparableConv2d(in_channels=4608, out_channels=4608) for _ in range(2)],
+    ResidualBlock4(in_channels=2048, out_channels=4096),
+    *[ResidualBlock4(in_channels=4096, out_channels=4096) for _ in range(2)],
 
     nn.AdaptiveAvgPool2d(1),
     nn.Flatten(),
     
-    nn.Linear(in_features=4608, out_features=2304),
+    nn.Linear(in_features=2048, out_features=2048),
     nn.ReLU(),
-    nn.LayerNorm(normalized_shape=2304),
+    nn.LayerNorm(normalized_shape=2048),
     
-    nn.Linear(in_features=2304, out_features=1152),
+    nn.Linear(in_features=2048, out_features=1024),
     nn.ReLU(),
-    nn.LayerNorm(normalized_shape=1152),
+    nn.LayerNorm(normalized_shape=1024),
 
-    nn.Linear(in_features=1152, out_features=512),
+    nn.Linear(in_features=1024, out_features=512),
     nn.ReLU(),
     nn.LayerNorm(normalized_shape=512),
     
     nn.Linear(in_features=512, out_features=2),
 )
 
-
-# superSepNetLarge = nn.Sequential(
+# Runs like twice as fast as previous ResNet, but looks like it will have worse performance# Also only uses 9.2 GB when compared to the 22 GB from ResNet 
+# TODO: Continue training this later, seems to not have fully fit yet.
+# After 15 epochs: Train: 92.414%, Val: 81.015%, Test: 81.035%
+# After 30 epochs: Train 99.72, Val: 82.99, Test: 82.6
+# superSepNetSmall = nn.Sequential(
 #     DoubleResidualDWSeparableConv2d(in_channels=3, out_channels=9),
 #     ResidualDownsampleSep(in_channels=9), # 112
     
 #     DoubleResidualDWSeparableConv2d(in_channels=9, out_channels=18),
-#     *[DoubleResidualDWSeparableConv2d(in_channels=18, out_channels=18) for _ in range(4)],
 #     ResidualDownsampleSep(in_channels=18), # 56
     
 #     DoubleResidualDWSeparableConv2d(in_channels=18, out_channels=36),
-#     *[DoubleResidualDWSeparableConv2d(in_channels=36, out_channels=36) for _ in range(4)],
 #     ResidualDownsampleSep(in_channels=36), # 28
     
 #     DoubleResidualDWSeparableConv2d(in_channels=36, out_channels=72),
 #     DoubleResidualDWSeparableConv2d(in_channels=72, out_channels=144),
 #     DoubleResidualDWSeparableConv2d(in_channels=144, out_channels=144),
-#     *[DoubleResidualDWSeparableConv2d(in_channels=144, out_channels=144) for _ in range(4)],
 #     ResidualDownsampleSep(in_channels=144), # 14
 
 #     DoubleResidualDWSeparableConv2d(in_channels=144, out_channels=288),
-#     *[DoubleResidualDWSeparableConv2d(in_channels=288, out_channels=288) for _ in range(4)],
+#     *[DoubleResidualDWSeparableConv2d(in_channels=288, out_channels=288) for _ in range(2)],
 #     ResidualDownsampleSep(in_channels=288), # 7
     
 #     DoubleResidualDWSeparableConv2d(in_channels=288, out_channels=576),
-#     *[DoubleResidualDWSeparableConv2d(in_channels=576, out_channels=576) for _ in range(4)],
+#     *[DoubleResidualDWSeparableConv2d(in_channels=576, out_channels=576) for _ in range(2)],
 #     ResidualDownsampleSep(in_channels=576), # 3
 
 #     DoubleResidualDWSeparableConv2d(in_channels=576, out_channels=1152),
-#     *[DoubleResidualDWSeparableConv2d(in_channels=1152, out_channels=1152) for _ in range(10)],
+#     *[DoubleResidualDWSeparableConv2d(in_channels=1152, out_channels=1152) for _ in range(5)],
 
 #     DoubleResidualDWSeparableConv2d(in_channels=1152, out_channels=2304),
-#     *[DoubleResidualDWSeparableConv2d(in_channels=2304, out_channels=2304) for _ in range(6)],
+#     *[DoubleResidualDWSeparableConv2d(in_channels=2304, out_channels=2304) for _ in range(3)],
 
 #     DoubleResidualDWSeparableConv2d(in_channels=2304, out_channels=4608),
-#     *[DoubleResidualDWSeparableConv2d(in_channels=4608, out_channels=4608) for _ in range(4)],
+#     *[DoubleResidualDWSeparableConv2d(in_channels=4608, out_channels=4608) for _ in range(2)],
 
 #     nn.AdaptiveAvgPool2d(1),
 #     nn.Flatten(),
@@ -669,23 +620,75 @@ superSepNetSmall = nn.Sequential(
 #     nn.Linear(in_features=512, out_features=2),
 # )
 
+# Didn't train at all, 7751 s train
+# Changing to NAdam and much lower LR without AMP got it training
+# 99.963 Train, 88.85 Val, 89.485 Test
+# Need to cut batch size to 32 with augmentations because they use too much memory lole
+# ABSOLUTELY GOATED WITH AUGS 98.65|96.74|96.64 TAKES FOREVER TO TRAIN: 16189s
+# superSepNetLarge = nn.Sequential(
+#     DoubleResidualDWSeparableConv2d(in_channels=3, out_channels=18),
+#     ResidualDownsampleSep(in_channels=18), # 112
+    
+#     DoubleResidualDWSeparableConv2d(in_channels=18, out_channels=36),
+#     ResidualDownsampleSep(in_channels=36), # 56
+    
+#     DoubleResidualDWSeparableConv2d(in_channels=36, out_channels=72),
+#     ResidualDownsampleSep(in_channels=72), # 28
+    
+#     DoubleResidualDWSeparableConv2d(in_channels=72, out_channels=144),
+#     ResidualDownsampleSep(in_channels=144), # 14
+
+#     DoubleResidualDWSeparableConv2d(in_channels=144, out_channels=288),
+#     ResidualDownsampleSep(in_channels=288), # 7
+    
+#     DoubleResidualDWSeparableConv2d(in_channels=288, out_channels=576),
+#     ResidualDownsampleSep(in_channels=576), # 3
+
+#     DoubleResidualDWSeparableConv2d(in_channels=576, out_channels=1152),
+#     *[DoubleResidualDWSeparableConv2d(in_channels=1152, out_channels=1152) for _ in range(6)],
+
+#     DoubleResidualDWSeparableConv2d(in_channels=1152, out_channels=2304),
+#     *[DoubleResidualDWSeparableConv2d(in_channels=2304, out_channels=2304) for _ in range(6)],
+
+#     DoubleResidualDWSeparableConv2d(in_channels=2304, out_channels=4608),
+#     *[DoubleResidualDWSeparableConv2d(in_channels=4608, out_channels=4608) for _ in range(8)],
+
+#     nn.AdaptiveAvgPool2d(1),
+#     nn.Flatten(),
+    
+#     nn.Linear(in_features=4608, out_features=2304),
+#     nn.LayerNorm(normalized_shape=2304),
+#     nn.GELU(),
+
+#     nn.Linear(in_features=2304, out_features=1152),
+#     nn.LayerNorm(normalized_shape=1152),
+#     nn.GELU(),
+
+#     nn.Linear(in_features=1152, out_features=512),
+#     nn.LayerNorm(normalized_shape=512),
+#     nn.GELU(),
+
+#     nn.Linear(in_features=512, out_features=2),
+# )
+
 
 def main():
     
     # Test model loading here
     # customModel = VisualizableSWIN()
-    customModel = superSepNetSmall
-    
-    profileModel(customModel, input_size=(4, 3, 224, 224))
+    # customModel = VisualizableVIT() # Can't profile this one
+    customModel = None
+    input_size = (64, 3, 224, 224)
+    profileModel(customModel, input_size=input_size)
     
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     
     customModel.to(device)
     
-    dummyInput = torch.randn((4, 3, 224, 224)).to(device)
+    dummyInput = torch.randn((input_size)).to(device)
     
     out = customModel(dummyInput)
-    print(out)
+    # print(out)
     print(f'{out.shape=}')
     
     # print(list(visualizableVIT.children()))
